e6422cbf3da8941c3547f3af5418d9f5f7a958f6
Kalyan720 <119833878+Kalyan720@users.noreply.github.com>
2024-06-11 20:16:34 +0530

    Add files via upload

---
+++ WebScraping.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+
+#Function to create a custom search engine with Google API key.
+
+def Google_search(query, api_key, cx):
+
+    #Accessing the customised search engine using the Args query, api_key, cx.
+    url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={api_key}&cx={cx}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        search_results = [item['link'] for item in data.get('items', [])]      
+        if search_results:
+            print("Search results:\n")
+            for link in search_results:
+                print(link)
+            web_page(search_results)
+        else:
+            print("No search results found.")
+    else:
+        print(f"Failed to fetch search results. Status code: {response.status_code}")
+
+#Function to scrape individual links obtained from the search engine.
+
+def web_page(links):
+
+    for i, link in enumerate(links):
+        try:
+            response = requests.get(link)           
+            if response.status_code == 200:
+                print(f"\nThe Link: '{link}' is accessible.")
+                scraped_data = BeautifulSoup(response.text, "html.parser")
+                # Writing the scraped data to a file
+                with open(f"scraped_content_{i}.html", "w", encoding="utf-8") as file:
+                    file.write(str(scraped_data))
+                print(f"Scraped data is saved to 'scraped_content_{i}.html'")
+            else:
+                print(f"\nFailed to fetch '{link}'. Status code: {response.status_code}")
+        except Exception as e:
+            print(f"\nAn error occurred while fetching '{link}': {e}")
+
+if __name__ == "__main__":
+    #API key to access search engine.
+    api_key = "AIzaSyDvfVjK6pnOdYAPf-UMi4etRi0W7Fz2spI"
+
+    #id of custom google search engine.
+    cx = "0169eec34796e4587"
+
+    query = input("Enter the Google search query: ")
+    Google_search(query, api_key, cx)
+
+
+"""Thus we successfully scrape the web page urls on google.com and open individual urls and scrape the data,
+   store everything in document format as results. """
